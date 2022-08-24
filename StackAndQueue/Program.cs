47f50629229316964d3487f5526781896aa7d81a
ALLTALE2027 <86816300+ALLTALE2027@users.noreply.github.com>
2022-08-25 00:38:37 +0530

    [Darshan]: Add uc-3 create queue

--- StackAndQueue/Program.cs
+++ StackAndQueue/Program.cs
@@ -5,12 +5,14 @@
         static void Main(string[] args)
         {
             var stack = new Stack();
-
+            var queue = new Queue();
 
             Console.WriteLine("Select choice from below");
             Console.WriteLine("Stack!!!");
             Console.WriteLine("1. Create stack");
             Console.WriteLine("2. Empty stack -- Peek and then Pop");
+            Console.WriteLine("Queue!!!");
+            Console.WriteLine("3. Create a queue");
             int choice = int.Parse(Console.ReadLine());
 
             switch (choice)
@@ -28,6 +30,12 @@
                     stack.DisplayStack();
                     stack.EmptyStack();         // this will empty stack one by one element
                     stack.DisplayStack();
+                    break;
+                case 3:
+                    queue.EnQueue(56);
+                    queue.EnQueue(30);
+                    queue.EnQueue(70);
+                    queue.ShowQueue();
                     break;
                 default:
                     Console.WriteLine("Select a proper choice");
---
+++ StackAndQueue/Queue.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackAndQueue
+{
+    internal class Queue
+    {
+        private Node Front;
+        private Node Rear;
+        int size = 0;
+
+        public void EnQueue(int data)
+        {
+            Node node = new Node(data);
+            if (Front == null && Rear == null)
+            {
+                Front = node;
+                Rear = node;
+            }
+            else
+            {
+                Rear.next = node;
+                Rear = node;
+            }
+            size++;
+            Console.WriteLine("Element is added to queue");
+        }
+
+        public void DeQueue()
+        {
+            int value = Front.data;
+            Front = Front.next;
+
+            if (Front == null)
+                Rear = null;
+
+            size--;
+            Console.WriteLine("Element with data {0} is removed from queue ", value);
+        }
+
+        public void ShowQueue()
+        {
+            Node temp = Front;
+            if (temp == null)
+                Console.WriteLine("Queue is empty");
+            else
+            {
+                Console.WriteLine("Queue elements");
+                while (temp != null)
+                {
+                    Console.Write(temp.data);
+                    if (temp.next != null)
+                        Console.Write("<-");
+                    temp = temp.next;
+                }
+                Console.WriteLine();
+                Console.WriteLine("Queue length is: " + size);
+                Console.WriteLine();
+            }
+        }
+    }
+}
+
