ba8c1d5cc74ec106647face0d257c67c6502a857
ALLTALE2027 <86816300+ALLTALE2027@users.noreply.github.com>
2022-08-25 00:30:21 +0530

    [Darshan]: Add uc-2 peek and pop elements from stack until it's empty

--- StackAndQueue/Program.cs
+++ StackAndQueue/Program.cs
@@ -10,6 +10,7 @@
             Console.WriteLine("Select choice from below");
             Console.WriteLine("Stack!!!");
             Console.WriteLine("1. Create stack");
+            Console.WriteLine("2. Empty stack -- Peek and then Pop");
             int choice = int.Parse(Console.ReadLine());
 
             switch (choice)
@@ -18,6 +19,14 @@
                     stack.Push(70);
                     stack.Push(30);
                     stack.Push(56);
+                    stack.DisplayStack();
+                    break;
+                case 2:
+                    stack.Push(70);
+                    stack.Push(30);
+                    stack.Push(56);
+                    stack.DisplayStack();
+                    stack.EmptyStack();         // this will empty stack one by one element
                     stack.DisplayStack();
                     break;
                 default:
