2f57b02b14d7aa4f623334112a1d280d2cb617e1
ALLTALE2027 <86816300+ALLTALE2027@users.noreply.github.com>
2022-08-25 00:18:03 +0530

    [Darshan]: Add UC-1 create stack

---
+++ StackAndQueue/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace StackAndQueue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var stack = new Stack();
+
+
+            Console.WriteLine("Select choice from below");
+            Console.WriteLine("Stack!!!");
+            Console.WriteLine("1. Create stack");
+            int choice = int.Parse(Console.ReadLine());
+
+            switch (choice)
+            {
+                case 1:
+                    stack.Push(70);
+                    stack.Push(30);
+                    stack.Push(56);
+                    stack.DisplayStack();
+                    break;
+                default:
+                    Console.WriteLine("Select a proper choice");
+                    break;
+            }
+
+
+
+        }
+    }
+}
---
+++ StackAndQueue/Stack.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackAndQueue
+{    
+    internal class Stack
+    {
+        private Node Top;
+        private int size = 0;
+
+
+        public void Push(int data)  
+        {
+            Node node = new Node(data);
+            if (Top == null)
+            {
+                Top = node;
+            }
+            else
+            {
+                node.next = Top;
+                Top = node;
+            }
+            size++;
+            Console.WriteLine("Element is Pushed");
+        }
+
+        public void Pop()
+        {
+            if (Top == null)
+            {
+                Console.WriteLine("Stack is empty, can not pop");
+                return;
+            }
+            int value = Top.data;
+            Top = Top.next;
+            size--;
+            Console.WriteLine("Element with data {0} is pop-ed", value);
+        }
+
+        public void EmptyStack()  // this methods peeks and pops until stack is empty
+        {
+            Node temp=Top;
+            while (temp != null)
+            {
+                Peek();
+                Pop();
+                temp=temp.next;
+            }
+        }
+        public void Peek()
+        {
+            if (Top == null)
+            {
+                Console.WriteLine("Stack is empty");
+                return;
+            }
+            int value = Top.data;
+            Console.WriteLine("Element on top is: " + value);
+        }
+
+        public void DisplayStack()
+        {
+            Node temp = Top;
+            if (temp == null)
+                Console.WriteLine("Stack is empty");
+            else
+            {
+                Console.WriteLine("Stack contents");
+                while (temp != null)
+                {
+                    Console.WriteLine(temp.data);
+                    if (temp.next != null)
+                        Console.WriteLine("|");
+                    temp = temp.next;
+                }
+
+                Console.WriteLine();
+                Console.WriteLine("Stack height is: " + size);
+                Console.WriteLine();
+            }
+        }
+    }
+}
